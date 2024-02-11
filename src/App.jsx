86c0ff2724c921566869036c7236ff827369e82f
muhammadIbrahim1-k <muhammad9122004@gmail.com>
2024-02-11 23:51:11 +0500

    Fixing bug

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,7 @@ function App() {
 
   return loading? 
   <Loading /> : (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-    <div className='w-full block'>
-      <Header />
-      <main>
-      {/* TODO:  <Outlet /> */}
-      </main>
-      <Footer />
-    </div>
-  </div>
+    <></>
   )
 }
 
