c45c412c64ea25475c5796aa904374f1f8e70af8
muhammadIbrahim1-k <muhammad9122004@gmail.com>
2024-02-13 02:27:15 +0500

    Logo and Loading Update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Loading() {
   return (
     <>
-    <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+    <div class="grid place-content-center h-screen">
         <img class="w-20 h-35 animate-spin" src="https://www.svgrepo.com/show/173880/loading-arrows.svg" alt="Loading icon"/>
     </div>
   </>
---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logoImage from '../images/logo.png';
+
+function Logo() {
+  return (
+    <div>
+        <img src={logoImage} alt='Logo' />
+    </div>
+  )
+}
+
+export default Logo
