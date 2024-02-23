6d2f6fe6b4361a419319d643e653bab8cbd40f62
muhammadIbrahim1-k <muhammad9122004@gmail.com>
2024-02-23 23:35:29 +0500

    HomeBlogCard needs updation

---
+++ src/components/HomeBlogCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import appwriteService from '../backend/config'
+import { Link, useNavigate } from 'react-router-dom'
+
+function HomeBlogCard({$id, userId, username, title, blogImage}) {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+        <Link to={`/profile/${userId}`}>
+            <div>
+                <div>{username}</div>
+            </div>
+        </Link>
+        <Link to={`/post/${$id}`}>
+            <div className='border border-white-600 max-w-lg w-5/6 max-h-full'>
+                {!blogImage? null: <div>
+                    <img src={appwriteService.getFilePreview(blogImage)} alt={title} className='border border-black h-80'/>
+                </div> }
+                <div className='bg-slate-100 text-[25px]'>
+                    <div>{title}</div>
+                </div>
+            </div>
+        </Link>
+    </div>
+  )
+}
+
+export default HomeBlogCard
