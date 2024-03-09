ba0f1a367f30ab88da4b2102598ce82d268e7aaf
muhammadIbrahim1-k <muhammad9122004@gmail.com>
2024-03-09 16:56:45 +0500

    Route Protection Component

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import Loading from './index'
+
+export default function ProtectedContainer({children, authentication = true}) {
+  const navigate = useNavigate()
+  const authStatus = useSelector( state => state.auth.status )
+  const [loader, setLoader] = useState(true)
+
+  useEffect( () => {
+    if ( authentication && authStatus !== authentication) {
+      navigate('/login')
+    } else if ( !authentication && authStatus !== authentication) {
+      navigate('/')
+    }
+    setLoader(false)
+  }, [authStatus, navigate, authentication] )
+    
+  return loader? <Loading/> : <>{children}</>
+}
